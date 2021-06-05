6e15fb53a96354d32dcc0ea46f1ef38d3f3cbc12
Roshni Joseph <roshnijosepht@gmail.com>
2021-06-05 11:07:10 +0530

    condition and loop

---
+++ task1.js
@@ -0,0 +1,25 @@
+var a = 10;
+i=0;
+
+while(i<a+1){
+    console.log(i);
+    i++;
+}
+
+var b = 15;
+if(a>b){
+    console.log("a is greater");
+
+}
+else if(b>a){
+    console.log("b is greater")
+}
+else{
+    console.log("a and b are equal")
+}
+
+for(i=0;i<a;i++){
+    console.log(i)
+}
+
+
